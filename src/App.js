49eb5dd9c819390da4e5494f49c79b3495723143
GCell-droid <niteshparihar104@gmail.com>
2025-02-04 02:03:27 +0530

    after routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Body from './components/Body';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="font-bold text-red-400 text-4xl">
-      <h1>Hello</h1>
+  return <Provider store={appStore}>
+    <div>
+      <Body >
+      </Body>
+
     </div>
-  );
+  </Provider>
 }
 
 export default App;
